16c90da3a5de4e2af22a8b1f74e43c2c23ad67d2
JonasHaas <haas.qt@gmail.com>
2023-04-04 15:20:43 +0200

    init commit

---
+++ primes.py
@@ -0,0 +1,21 @@
+numbers = range(2, 251)
+count = 0
+filename = 'result.txt'
+
+with open(filename, 'w') as file:
+    # Open the file in "write" mode to clear its contents
+    pass
+
+with open(filename, 'a') as file:
+    for num in numbers:
+        for i in range(2, num):
+            if num % i == 0:
+                # num is not prime
+                break
+        else:
+
+            # num is prime, write it to the file
+            count += 1
+            file.write(str(num) + '\n')
+            
+print(str(count) + " prime numbers found.")
